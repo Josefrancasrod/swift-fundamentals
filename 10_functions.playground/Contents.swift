2d91b8065055dd268545202031d3cc9bd380a37e
José Francisco Castillo Rodríguez <josefrancasrod@gmail.com>
2021-07-13 23:41:23 -0500

    Fun with functions

--- 10_functions.playground/Contents.swift
+++ 10_functions.playground/Contents.swift
@@ -6,3 +6,62 @@ func greeting(person: String) -> String{
 }
 
 greeting(person: "José Francisco")
+
+
+func sayHelloWorld() -> String {
+    return "Hello World"
+}
+
+func greeting(person: String, isMale: Bool) -> String{
+    if isMale{
+        return "Bienvenido Caballero \(person)"
+    }else{
+        return "Bienvenida Dama \(person)"
+    }
+}
+
+greeting(person: "Jose", isMale: true)
+greeting(person: "Maria", isMale: false)
+
+func greet2(person: String){
+    print("\(person)")
+}
+
+greet2(person: "Jose")
+greet2(person: "Francisco")
+
+
+func printAndCount(string: String) -> Int {
+    print(string)
+    return string.count
+}
+
+printAndCount(string: "Holaaaa")
+
+func printWithoutCounting(string: String){
+    let _ = printAndCount(string: string)
+}
+
+printAndCount(string: "Holaaaaaa")
+printWithoutCounting(string: "Holaaaaaa")
+
+func minMax(arrays:[Int]) -> (min: Int, max: Int)? {
+    var currentMin = arrays[0]
+    var currenMax = arrays[0]
+    
+    if arrays.isEmpty { return nil }
+    
+    for value in arrays[1..<arrays.count]{
+        if value < currentMin {
+            currentMin = value
+        } else if value > currenMax{
+            currenMax = value
+        }
+    }
+    return (currentMin, currenMax)
+}
+
+
+let bounds = minMax(arrays: [1,5,2,6,3,5,8,2,7])
+print("Los valores se halla entre \(bounds!.min) y \(bounds!.max)")
+
