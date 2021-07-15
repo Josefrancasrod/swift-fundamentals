afb9708557f05e5658a288962f127e6ac4588e37
José Francisco Castillo Rodríguez <josefrancasrod@gmail.com>
2021-07-14 23:53:55 -0500

    Add functions with labers and variadics

--- 10_functions.playground/Contents.swift
+++ 10_functions.playground/Contents.swift
@@ -65,3 +65,33 @@ func minMax(arrays:[Int]) -> (min: Int, max: Int)? {
 let bounds = minMax(arrays: [1,5,2,6,3,5,8,2,7])
 print("Los valores se halla entre \(bounds!.min) y \(bounds!.max)")
 
+
+func someFunction(f1 firsParamName: Int, f2 secondParamName: Int = 6){
+    // asdasdasdas
+    print(firsParamName + secondParamName)
+}
+
+
+someFunction(f1: 1, f2: 2)
+
+
+func greeting(_ person: String, from hometown: String) -> String {
+    return "Hola \(person) de \(hometown)"
+}
+
+greeting("José Francisco", from: "Mexico")
+
+someFunction(f1: 5, f2: 1)
+someFunction(f1: 5)
+
+func mean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers{
+        total += number
+    }
+    return total/Double(numbers.count)
+}
+
+mean(1,2,3,4,5,6)
+
+
